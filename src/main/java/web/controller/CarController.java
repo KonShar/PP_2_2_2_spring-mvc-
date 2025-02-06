ad5f53c22e8d62b1507be593c07c13a408cb2278
Konstantin <kon.sharanin@gmail.com>
2025-02-06 11:18:31 +0300

    Optimized imports

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,14 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAO;
-import web.models.Car;
 import web.service.CarService;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
